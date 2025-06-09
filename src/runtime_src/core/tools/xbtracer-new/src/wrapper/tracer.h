da8aa595c4c6fbe4368a9dafa1e3b5733e464569
Wendy Liang <wendy.liang@amd.com>
2025-06-09 11:29:30 -0700

    xbtracer: add basic tracer class implementation
    add basic tracer class implementation to output tracing protobuf
    message to a file.
    
    Signed-off-by: Wendy Liang <wendy.liang@amd.com>

---
+++ src/runtime_src/core/tools/xbtracer-new/src/wrapper/tracer.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+#ifndef tracer_h
+#define tracer_h
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+
+extern "C" const char* func_mangled_map[];
+
+namespace xrt::tools::xbtracer
+{
+
+class tracer
+{
+  enum class level
+  {
+    DEFAULT = 0,
+  };
+
+public:
+  tracer(const std::string& outf, level tl);
+
+  // we always need to output tracing to a file
+  tracer() = delete;
+  // delete copy constructor and assignment operator to enforce singleton
+  tracer(const tracer&) = delete;
+  tracer& operator=(const tracer&) = delete;
+
+  ~tracer();
+
+  template <typename protobuf_msg>
+  bool
+  write_protobuf_msg(const protobuf_msg& msg)
+  {
+    return msg.SerializeToOstream(&tracer_ofile);
+  }
+
+  static
+  tracer*
+  get_instance();
+
+private:
+  static std::unique_ptr<tracer> instance;
+  static std::once_flag init_instance_flag;
+  std::fstream tracer_ofile;
+  level tlevel;
+}; // class xrt::tools::xbracer::tracer
+
+} // namespace xrt::tools::xbtracer
+
+template <typename protobuf_msg>
+bool
+xbtracer_write_protobuf_msg(const protobuf_msg& msg)
+{
+  return xrt::tools::xbtracer::tracer::get_instance()->write_protobuf_msg(msg);
+}
+
+#endif // tracer_h

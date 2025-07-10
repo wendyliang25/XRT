a721f4ff6c7040bdfd53a87b839d20d85893152e
Wendy Liang <wendy.liang@amd.com>
2025-07-10 14:26:14 -0700

    xbtracer: common: trace_utils: indentation
    Signed-off-by: Wendy Liang <wendy.liang@amd.com>

--- src/runtime_src/core/tools/xbtracer-new/src/common/trace_utils.cpp
+++ src/runtime_src/core/tools/xbtracer-new/src/common/trace_utils.cpp
@@ -16,9 +16,7 @@ get_func_mname_from_signature(const char* s)
   for (uint32_t i = 0; i < get_size_of_func_mangled_map(); i += 2)
   {
     if (!strcmp(s, func_mangled_map[i]))
-    {
       return func_mangled_map[++i];
-    }
   }
   return nullptr;
 }
